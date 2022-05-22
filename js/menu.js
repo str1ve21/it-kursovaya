95de875f926bad139129b3948ce28a17c7eb96bc
str1ve <end3avour21@gmail.com>
2022-05-22 11:50:35 +0700

    menu animations

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,33 @@ const switcher = document.querySelector('#menu-switch');
 const switcherMenu = document.querySelector('#menu-nav');
 let isHidden = true;
 
+const svgStripe = document.querySelector('#svg-stripe');
+const svgX = document.querySelector('#svg-x');
+
 switcher.addEventListener('click', () => {
     if (isHidden) {
         isHidden = false;
-        switcherMenu.classList.remove('hidden');
+        switcherMenu.classList.remove('opacity-0');
+        switcherMenu.classList.add('opacity-100');
+
+        svgStripe.classList.remove('opacity-100');
+        svgStripe.classList.add('opacity-0');
+
+        svgX.classList.remove('opacity-0');
+        svgX.classList.add('opacity-100');
         return;
     };
 
     if (isHidden === false) {
         isHidden = true;
-        switcherMenu.classList.add('hidden');
+        switcherMenu.classList.remove('opacity-100');
+        switcherMenu.classList.add('opacity-0');
+
+        svgStripe.classList.remove('opacity-0');
+        svgStripe.classList.add('opacity-100');
+
+        svgX.classList.remove('opacity-100');
+        svgX.classList.add('opacity-0');
         return;
     };
 });
