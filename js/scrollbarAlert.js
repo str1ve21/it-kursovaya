8b6e7ad65124dae1421aaf19caa4a6f6d66c215e
str1ve <end3avour21@gmail.com>
2022-05-22 13:37:50 +0700

    alert button

--- js/scrollbarAlert.js
+++ js/scrollbarAlert.js
@@ -1,17 +1,23 @@
 const mainCont = document.querySelector('.main-cont');
 
 const scrollbarAlert = document.querySelector('#scrollbar-alert');
+const scrollbarSwitch = document.querySelector('#scroll-switch');
 const alertCloseSvg = document.querySelector('#svg-alert-x');
 const alertCloseBtn = document.querySelector('#scroll-off');
 const alertAddScroll = document.querySelector('#scroll-on');
-let isHiddenAlert = false;
 
 function closeAlert() {
     scrollbarAlert.classList.remove('opacity-100');
     scrollbarAlert.classList.add('opacity-0');
     setTimeout(() => {
-        scrollbarAlert.classList.add('hidden');
+        scrollbarAlert.classList.add('-z-50');
     }, 300);
+    return;
+};
+
+function removeScroll() {
+    mainCont.classList.add('scrollbar-none');
+    closeAlert();
     return;
 };
 
@@ -21,12 +27,17 @@ function addScroll() {
     return;
 };
 
-alertCloseSvg.addEventListener('click', closeAlert);
-alertCloseBtn.addEventListener('click', closeAlert);
-alertAddScroll.addEventListener('click', addScroll);
-
-setTimeout(() => {
+function openAlert() {
+    scrollbarAlert.classList.remove('-z-50');
     scrollbarAlert.classList.remove('opacity-0');
     scrollbarAlert.classList.add('opacity-100');
-    return;
-}, 4000);
+    clearTimeout(autoOpenAlert);
+    return; 
+};
+
+alertCloseSvg.addEventListener('click', closeAlert);
+alertCloseBtn.addEventListener('click', removeScroll);
+alertAddScroll.addEventListener('click', addScroll);
+scrollbarSwitch.addEventListener('click', openAlert);
+
+const autoOpenAlert = setTimeout(openAlert, 4000);
