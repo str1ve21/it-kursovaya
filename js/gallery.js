211c5dea0402757ce6df9ed917bd0ed7d0a12492
str1ve <end3avour21@gmail.com>
2022-05-23 16:32:26 +0700

    aboutMe ready

--- js/gallery.js
+++ js/gallery.js
@@ -2,16 +2,18 @@ const galleryImg = document.querySelector('#gallery-image');
 const plusImg = document.querySelector('#gallery-img-plus');
 const minusImg = document.querySelector('#gallery-img-minus');
 
+const currentSiteFolder = document.querySelector('.galleryMatch').id;
+
 let currentImg = 1;
 
-galleryImg.src = '../assets/myClassGallery/img1.webp';
+galleryImg.src = `../assets/${currentSiteFolder}/img1.webp`;
 
 function eventPlusImg() {
     currentImg += 1;
     if (currentImg === 5) {
         currentImg = 1;
     };
-    galleryImg.src = `../assets/myClassGallery/img${currentImg}.webp`;
+    galleryImg.src = `../assets/${currentSiteFolder}/img${currentImg}.webp`;
 };
 
 function eventMinusImg() {
@@ -19,7 +21,7 @@ function eventMinusImg() {
     if (currentImg === 0) {
         currentImg = 4;
     };
-    galleryImg.src = `../assets/myClassGallery/img${currentImg}.webp`;
+    galleryImg.src = `../assets/${currentSiteFolder}/img${currentImg}.webp`;
 };
 
 plusImg.addEventListener('click', eventPlusImg);
