2e4145e4fd62214e60c01acf63045f189230767f
str1ve <end3avour21@gmail.com>
2022-05-23 19:38:40 +0700

    test base

--- js/checkScroll.js
+++ js/checkScroll.js
@@ -1,7 +1,6 @@
 const mainCont = document.querySelector('.main-cont');
 
 let isScrollAdded = sessionStorage.getItem('isScrollAdded');
-console.log(isScrollAdded)
 
 if (isScrollAdded === 'true') {
     mainCont.classList.remove('scrollbar-none');
